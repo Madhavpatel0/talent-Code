d14aa87bdeaa346928fa05bba932cf20c66810e8
Madhav Patel <0112it221031@gmail.com>
2026-05-19 17:43:01 +0530

    middleware fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,15 +8,11 @@ import { inngest, functions } from "./lib/inngest.js";
 
 const app = express();
 
+app.use(express.json());
+
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 
-app.use(
-  "/api/inngest",
-  serve({
-    client: inngest,
-    functions,
-    signingKey: ENV.INNGEST_SIGNING_KEY,  })
-);
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const __dirname = path.resolve();
 
